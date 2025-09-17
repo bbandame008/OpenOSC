992a5a441f93d48bb6d9b0ca1027d197096bba52
Bhuvana Chandra Bandameedi <bbandame@cisco.com>
2025-09-17 21:46:12 +0530

    test

--- src/openosc_support.c
+++ src/openosc_support.c
@@ -56,7 +56,7 @@ openosc_get_process_name(char *name, int name_len)
     if (fd != NULL) {
         size_t size = fread((void *)name, sizeof(char), name_len, fd);
 	if (size > 0) {
-	    name[size]='\0';
+	    name[size-1]='\0';
 	}
 	fclose(fd);
         return TRUE;
